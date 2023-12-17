8e7a2e273670c7add9a5838ef4045f4936284817
ali <alilaser8080@gmail.com>
2023-12-17 23:35:35 +0330

    version2: calc_create_theme added

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import PySimpleGUI as sg
 
-sg.theme("graygraygray")
-sg.set_options(font="Calibre 14", button_element_size=(6, 3))
-button_size = (6, 3)
-button_size2 = (12, 3)
-layout = [
-    # [sg.Push(), sg.Text("Output", font="Calibre 30")],
-    [sg.Text("Output", font="Calibre 30", expand_x=True, justification="right", pad=(20, 10))],
-        [sg.Button("Clear", key="-CLEAR-", size=button_size2, expand_x=True), sg.Button("Enter", key="-ENTER-", size=button_size2, expand_x=True)],
-    [sg.Button(7, size=button_size), sg.Button(8, size=button_size), sg.Button(9, size=button_size), sg.Button("*", size=button_size)],
-    [sg.Button(4, size=button_size), sg.Button(5, size=button_size), sg.Button(6, size=button_size), sg.Button("/", size=button_size)],
-    [sg.Button(1, size=button_size), sg.Button(2, size=button_size), sg.Button(3, size=button_size), sg.Button("-", size=button_size)],
-    [sg.Button(0, expand_x=True), sg.Button(".", size=button_size), sg.Button("+", size=button_size)],
-]
 
-window = sg.Window("Calculator", layout)
+def create_window(theme):
+    """
+    A method for determine and create a new window for calculator
+    :param theme:
+    :return:
+    """
+    sg.theme(theme)
+    sg.set_options(font="Calibre 14", button_element_size=(6, 3))
+    button_size = (6, 3)
+    button_size2 = (12, 3)
+    layout = [
+        # [sg.Push(), sg.Text("Output", font="Calibre 30")],
+        [sg.Text("Output", font="Calibre 30", expand_x=True, justification="right", pad=(20, 10), right_click_menu=theme_menu)],
+        [sg.Button("Clear", key="-CLEAR-", size=button_size2, expand_x=True), sg.Button("Enter", key="-ENTER-", size=button_size2, expand_x=True)],
+        [sg.Button(7, size=button_size), sg.Button(8, size=button_size), sg.Button(9, size=button_size), sg.Button("*", size=button_size)],
+        [sg.Button(4, size=button_size), sg.Button(5, size=button_size), sg.Button(6, size=button_size), sg.Button("/", size=button_size)],
+        [sg.Button(1, size=button_size), sg.Button(2, size=button_size), sg.Button(3, size=button_size), sg.Button("-", size=button_size)],
+        [sg.Button(0, expand_x=True), sg.Button(".", size=button_size), sg.Button("+", size=button_size)],
+    ]
+    return sg.Window("Calculator", layout)
+
+
+theme_menu = ["menu", ["Black", "dark", "BlueMono", "random"]]
+window = create_window("Black")
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-
 
 window.close()
